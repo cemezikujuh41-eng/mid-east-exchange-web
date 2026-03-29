01bc6c3f2381130be84c890469f15daf365051a7
吴百万 <cemezikujuh41-eng@users.noreply.github.com>
2026-03-29 18:47:29 +0800

    feat: aurora frontend refresh

---
+++ env.js
@@ -0,0 +1,4 @@
+window.ENV = {
+  SUPABASE_URL: 'https://mezvbnvfthnvzgpwjejq.supabase.co',
+  SUPABASE_ANON_KEY: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1lenZibnZmdGhudnpncHdqZWpxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzQ1MTAxMzcsImV4cCI6MjA5MDA4NjEzN30.-qXBO0ANxxNds6vhBWLDSH3F6fWFRSvkR15zPQIJkOo'
+};
